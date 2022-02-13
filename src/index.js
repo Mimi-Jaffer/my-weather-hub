682f97eacb0453ca9ff05243b647d69427147a2f
Mimi-Jaffer <94391046+Mimi-Jaffer@users.noreply.github.com>
2022-02-13 22:06:18 +0000

    Added background for days of the week back in

--- src/index.js
+++ src/index.js
@@ -51,7 +51,7 @@ function displayForecast(response) {
                 forecastDay.weather[0].icon
               }@2x.png"
               alt=""
-              width="42"
+              width="50"
             />
             <div class="weather-forecast-temperatures">
               <span class="weather-forecast-temparature-max">${Math.round(
